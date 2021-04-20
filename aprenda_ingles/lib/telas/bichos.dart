c58418080f723c3859a772b333238cafac878f24
JETOSBALDO\papul <papulelo10@gmail.com>
2021-04-20 11:05:43 -0300

    Comitando mudanças

--- aprenda_ingles/lib/telas/bichos.dart
+++ aprenda_ingles/lib/telas/bichos.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class Bichos extends StatefulWidget {
@@ -6,10 +8,78 @@ class Bichos extends StatefulWidget {
 }
 
 class _BichosState extends State<Bichos> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  final AudioCache _audioCache = AudioCache();
+
+  _executar(String nomeAudio) async {
+    audioPlayer = await _audioCache.play("audios/" + nomeAudio + ".mp3");
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _audioCache.loadAll([
+      "cao.mp3",
+      "gato.mp3",
+      "leao.mp3",
+      "ovelha.mp3",
+      "macaco.mp3",
+      "vaca.mp3"
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Bichos"),
+    //classe para deixar os icones adaptaveis em diferentes telas
+    //Assim nós podemos capturar os dados da tela do celular onde
+    //o aplicativo está rodando.
+    //double largura = MediaQuery.of(context).size.width;
+    //double altura = MediaQuery.of(context).size.height;
+    //Ou - > MediaQuery.of(Context).size.aspectRatio
+    return GridView.count(
+      crossAxisCount: 2,
+      //tamanho dos ícones de acordo com a tela
+      //foi multiplicado por 2 pois ainda estava muito pequeno
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _executar("cao");
+          },
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("gato");
+          },
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("leao");
+          },
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("ovelha");
+          },
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("macaco");
+          },
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: () {
+            _executar("vaca");
+          },
+          child: Image.asset("assets/images/vaca.png"),
+        ),
+      ],
     );
   }
 }
